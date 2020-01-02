7d70a1a8d8705cd8477907bbe356c69b98b2c850
strimnal32246 <susan.trimnal@icdccollege.edu>
2020-01-02 12:23:22 -0500

    added navbar and signup

---
+++ components/SIgnin.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const SignIn = () => {
+    const {user, setUser} = useState({
+        username: ''
+    })
+    return (
+        <section>
+            <input>
+        </section>
+    )
+}
